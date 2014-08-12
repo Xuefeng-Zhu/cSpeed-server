14f600a60e39aa2f795126ed911a7955b59fa6c8
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-12 01:19:21 -0400

    test_firebase

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+
+var Firebase = require('firebase');
+var fb = new Firebase('https://speedtest.firebaseio.com/');
+var fbvalue = null;
+fb.child('individuals').on("value", function(dataSnapshot) {
+	fbvalue = dataSnapshot.val();
+});;
+
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send(fbvalue);
 });
 
 var port = Number(process.env.PORT || 5000);
