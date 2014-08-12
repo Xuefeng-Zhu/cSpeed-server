791b6a364bab8af311814be0278e212c9e19c8bb
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-12 01:25:40 -0400

    test_firebase

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var Firebase = require('firebase');
 var fb = new Firebase('https://speedtest.firebaseio.com/');
 
 fb.child('individuals').on("value", function(dataSnapshot) {
-	console.log(dataSnapshot.val())
+	console.log(dataSnapshot.name())
 });;
