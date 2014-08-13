14e550eb3e3cef6fb718d6c05f947adc92b4421f
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-13 06:21:17 -0400

    test_port

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 var Firebase = require('firebase');
 
 var fb = new Firebase('https://speedtest.firebaseio.com/');
+var app = express();
 
 fb.child('individuals').on("value", function(dataSnapshot) {
     console.log(dataSnapshot.name())
-});;
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
