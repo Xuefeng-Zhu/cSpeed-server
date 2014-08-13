efc9eb2351f9c55096e8fc57d1fd8d3523b80276
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-13 06:18:40 -0400

    test

--- web.js
+++ web.js
@@ -1,31 +1,7 @@
 var Firebase = require('firebase');
+
 var fb = new Firebase('https://speedtest.firebaseio.com/');
-var total = {
-    count: 0
-};
 
 fb.child('individuals').on("value", function(dataSnapshot) {
-    var data = dataSnapshot.val();
-    for (var i in data) {
-        var test = data[i]
-        if (total.median) {
-            total.median.push(0);
-        } else {
-            total.median = [0];
-        }
-        for (var site in test) {
-            if (site != "user_info") {
-                var time = test[site].time
-                var load_time = time.loadEventEnd - time.navigationStart;
-                if (total[site]) {
-                    total[site].push(load_time);
-                } else {
-                    total[site] = [load_time];
-                }
-                total.median[total.count] += load_time;
-            }
-        }
-        total.count += 1;
-    }
-    fb.child('test').set(total);
+    console.log(dataSnapshot.name())
 });;
