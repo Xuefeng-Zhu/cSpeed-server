b681baa0b45001457d5a5e01ac404adee566a234
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-14 01:09:14 -0400

    test_region

--- web.js
+++ web.js
@@ -8,39 +8,82 @@ var total = {
     count: 0
 };
 
+var region = {
+
+};
+
 fb.child('individuals').on("value", function(dataSnapshot) {
     var data = dataSnapshot.val();
     for (var i in data) {
-        var test = data[i]
+        var test = data[i];
+        var ip = test.user_info.ip;
+
         if (total.median) {
             total.median.push(0);
         } else {
             total.median = [0];
         }
+
+        if (region[ip.city] == undefined) {
+            region[ip.city] = {
+                count: 0
+            };
+        }
+
+        if (region[ip.city][ip.isp] == undefined) {
+            region[ip.city][ip.isp] = {
+                median: [0],
+                count: 0
+            };
+        }
+        else{
+            region[ip.city][ip.isp].median.push(0);
+        }
+
         for (var site in test) {
             if (site != "user_info") {
                 var time = test[site].time
                 var load_time = time.loadEventEnd - time.navigationStart;
+
+                //load data into total 
                 if (total[site]) {
                     total[site].push(load_time);
                 } else {
                     total[site] = [load_time];
                 }
                 total.median[total.count] += load_time;
+
+                //load data into region
+                var temp = region[ip.city][ip.isp];
+                temp.median[temp.count] += load_time;
             }
         }
         total.count += 1;
+        region[ip.city].count += 1;
+        region[ip.city][ip.isp].count += 1;
     }
-    for (var i in total){
-        if (i != "count"){
+    //find median value for total
+    for (var i in total) {
+        if (i != "count") {
             total[i].sort();
-            fb.child('test/' + i).set(total[i][Math.floor(total[i].length/2)]);
+            fb.child(['test',i].join('/')).set(total[i][Math.floor(total[i].length / 2)]);
         }
     }
     fb.child('test/child').set(total.count);
+
+    //find median for region
+    for (var i in region){
+        var city = region[i];
+        for (var isp in city){
+            if (isp != "count"){
+                city[isp].median.sort();
+                fb.child(['region', i, isp, 'median'].join('/')).set(city[isp].median[Math.floor(city[isp].count/2)]);
+            }
+        }
+    }
 });
 
 var port = Number(process.env.PORT || 7000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
